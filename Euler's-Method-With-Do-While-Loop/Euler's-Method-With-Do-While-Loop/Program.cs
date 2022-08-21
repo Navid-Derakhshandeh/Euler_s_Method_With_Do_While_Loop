5fc0ad4a99315aa2f5d5b45f4d27754854547ed2
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2022-08-21 14:54:38 -0700

    Add files via upload

---
+++ Euler's-Method-With-Do-While-Loop/Euler's-Method-With-Do-While-Loop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_s_Method_With_Do_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100000;
+            do
+            {
+                Console.WriteLine("Please Enter the Time : ");
+
+                double x = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please Enter your point");
+
+                double y = Double.Parse(Console.ReadLine());
+
+                double z = 0.2 * y;
+
+                double e = 1 - x;
+
+                double s = e / 100;
+
+                double a = z * s;
+
+                double r = a * x;
+
+                double b = y + r;
+
+                Console.WriteLine("your next point is : {0}", b);
+
+                i--;
+            }
+            while (i > 0);
+            Console.Read();
+        }
+    }
+}
